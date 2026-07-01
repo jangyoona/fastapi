8ead5c93a77b82bdac6a79f1e51f3b8bc1674302
jangyoona <olozg@naver.com>
2026-07-01 16:16:58 +0900

    chore: 로그 레벨 및 형식 설정

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from routers.main_router import router as main_router
 from routers.weather_router import router as weather_router
 from routers.gemini_router import router as gemini_router
 from exceptions.handlers import register_exception_handlers
+import logging
 
 app = FastAPI()
 
@@ -11,3 +12,9 @@ register_exception_handlers(app)
 app.include_router(main_router)
 app.include_router(weather_router)
 app.include_router(gemini_router)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s    %(levelname)-8s    (%(thread)d)    --   %(name)-30s  :  %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
